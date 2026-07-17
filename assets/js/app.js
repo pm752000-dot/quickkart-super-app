6d00a7a56597a1765691f9beb50e2d511936bd73
pm752000-dot <pm752000@gmail.com>
2026-07-17 13:05:13 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
-alert("QuickKart JS Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+  alert("QuickKart Working");
+
+  const buttons = document.querySelectorAll(".product button");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+
+      const product = button.closest(".product");
+      const name = product.querySelector("h3").textContent;
+      const price = product.querySelector("p").textContent;
+
+      alert(name + "\n" + price + "\nAdded to Cart 🛒");
+
+    });
+  });
+
+});
