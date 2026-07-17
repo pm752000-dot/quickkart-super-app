59a0fc32e1df289662dc091aa09febbf1ae3ed94
pm752000-dot <pm752000@gmail.com>
2026-07-17 12:06:02 +0530

    Update app.js

--- assets/assets/js/app.js
+++ assets/assets/js/app.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("QuickKart Super App Loaded Successfully!");
+
+  console.log("QuickKart App Loaded");
+
+  let buttons = document.querySelectorAll(".product button");
+
+  buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+      let product = button.parentElement;
+
+      let name = product.querySelector("h3").innerText;
+      let price = product.querySelector("p").innerText;
+
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      cart.push({
+        name: name,
+        price: price
+      });
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      alert(name + " added to cart 🛒");
+
+    });
+
+  });
+
 });
